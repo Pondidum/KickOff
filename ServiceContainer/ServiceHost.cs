d8a63c09544097947992457651e587bff6bdfcf0
andy <andy@andydote.co.uk>
2016-07-05 19:33:01 +0100

    initial version

---
+++ ServiceContainer/ServiceHost.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ServiceProcess;
+
+namespace ServiceContainer
+{
+	public class ServiceHost
+	{
+		public static void Run(string name, Action entryPoint)
+		{
+			if (Environment.UserInteractive)
+			{
+				Console.WriteLine("Running Console...");
+				Console.WriteLine("Press any key to exit");
+
+				entryPoint();
+
+				Console.ReadKey();
+			}
+			else
+			{
+				var service = new ServiceWrapper(name, entryPoint);
+
+				ServiceBase.Run(service);
+			}
+		}
+	}
+
+	internal class ServiceWrapper : ServiceBase
+	{
+		private readonly Action _entryPoint;
+
+		public ServiceWrapper(string name, Action entryPoint)
+		{
+			_entryPoint = entryPoint;
+			ServiceName = name;
+		}
+
+		protected override void OnStart(string[] args)
+		{
+			_entryPoint();
+		}
+	}
+}
---
+++ TestService/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceContainer;
+
+namespace TestService
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			ServiceHost.Run("TestService", () =>
+			{
+				File.AppendAllLines(@"D:\dev\test-projects\ServiceContainer\\log.txt", new [] { "boot!" });
+			});
+		}
+	}
+}
