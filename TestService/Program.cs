0ccb3482807887d25306b74e0fbc4b149fcae4d6
andy <andy@andydote.co.uk>
2016-07-06 23:24:32 +0100

    program: implement startup class

--- TestService/Program.cs
+++ TestService/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using ServiceContainer;
 
@@ -13,10 +14,26 @@ namespace TestService
 		static void Main(string[] args)
 		{
 
-			ServiceHost.Run("TestService", () =>
-			{
-				File.AppendAllLines(@"D:\dev\test-projects\ServiceContainer\\log.txt", new [] { "boot!" });
-			});
+			ServiceHost.Run<LogWriter>("TestService");
+
+			Thread.Sleep(5000);
+		}
+	}
+
+	public class LogWriter : IStartup, IDisposable
+	{
+		public LogWriter()
+		{
+			Console.WriteLine("starting up");
+		}
+		public void Execute()
+		{
+			File.AppendAllLines(@"D:\dev\test-projects\ServiceContainer\\log.txt", new[] { "boot!" });
+		}
+
+		public void Dispose()
+		{
+			Console.WriteLine("shutting down");
 		}
 	}
 }
