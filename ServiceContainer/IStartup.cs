7b797da5165912f991670fb9f1a325f960d920d4
andy <andy@andydote.co.uk>
2016-07-07 23:28:20 +0100

    serviceArgs: split into separate file

--- ServiceContainer/IStartup.cs
+++ ServiceContainer/IStartup.cs
@@ -1,21 +1,7 @@
-﻿using System;
-
-namespace ServiceContainer
+﻿namespace ServiceContainer
 {
 	public interface IStartup
 	{
 		void Execute(ServiceArgs service);
-	}
-
-	public class ServiceArgs
-	{
-		public bool CancelRequested => _checkForCancel();
-
-		private readonly Func<bool> _checkForCancel;
-
-		public ServiceArgs(Func<bool> checkForCancel)
-		{
-			_checkForCancel = checkForCancel;
-		}
 	}
 }
---
+++ ServiceContainer/ServiceArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ServiceContainer
+{
+	public class ServiceArgs
+	{
+		public bool CancelRequested => _checkForCancel();
+
+		private readonly Func<bool> _checkForCancel;
+
+		public ServiceArgs(Func<bool> checkForCancel)
+		{
+			_checkForCancel = checkForCancel;
+		}
+	}
+}
