342abe86f44a6809b5dee6035005c297bd87ac97
andy <andy@andydote.co.uk>
2016-07-15 23:48:08 +0100

    testService: add consul and logging config

--- TestService/Program.cs
+++ TestService/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Consul;
 using ServiceContainer;
 
 namespace TestService
@@ -15,16 +16,37 @@ namespace TestService
 		{
 
 			ServiceHost.Run<LogWriter>("TestService");
-
-			Thread.Sleep(5000);
 		}
 	}
 
-	public class Config
+	public class Config : ILogConfig, IConsulRegistration
 	{
-		public string LoggingEndpoint { get; set; }
-		public bool EnableKibana { get; set; }
+		private readonly AgentService _service;
+
+		public Config()
+		{
+			_service = new AgentService
+			{
+				Address = "http://localhost",
+				Port = 8005,
+				Service = "TestService"
+			};
+		}
+
+		public bool EnableKibana { get; }
+		public Uri LoggingEndpoint { get; }
+
+		public CatalogRegistration CreateRegistration()
+		{
+			return new CatalogRegistration() { Service = _service };
+		}
+
+		public CatalogDeregistration CreateDeregistration()
+		{
+			return new CatalogDeregistration { ServiceID = _service.ID };
+		}
 	}
+
 	public class LogWriter : IStartup, IDisposable
 	{
 		public LogWriter()
@@ -37,7 +59,7 @@ namespace TestService
 			File.AppendAllLines(@"D:\dev\test-projects\ServiceContainer\\log.txt", new[] { "boot!" });
 
 
-			while(service.CancelRequested == false)
+			while (service.CancelRequested == false)
 				Thread.Sleep(500);
 		}
 
