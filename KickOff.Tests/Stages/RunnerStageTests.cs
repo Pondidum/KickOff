2b88b9c5e12bb82a4db78fbbca27702e16d93d91
andy <andy@andydote.co.uk>
2016-11-06 19:54:10 +0200

    runnerStage: add tests

---
+++ KickOff.Tests/Stages/RunnerStageTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using KickOff.Stages;
+using NSubstitute;
+using Xunit;
+
+namespace KickOff.Tests.Stages
+{
+	public class RunnerStageTests
+	{
+		private readonly RunnerStage _runner;
+
+		public RunnerStageTests()
+		{
+			_runner = new RunnerStage(Enumerable.Empty<string>().ToArray());
+		}
+
+		[Fact]
+		public void Execute_returns_immediately()
+		{
+			var startup = Substitute.For<IStartup>();
+
+			_runner.InstanceFactory = type => startup;
+			_runner.Execute();
+
+			Thread.Sleep(50);
+
+			startup.Received().Execute(Arg.Any<ServiceArgs>());
+		}
+	}
+}
