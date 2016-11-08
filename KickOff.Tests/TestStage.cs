6b2d9f8cf819f0e9b32dbdd76e2af6c28ddfc393
andy <andy@andydote.co.uk>
2016-11-08 18:18:01 +0200

    tests: make testStage shared by all tests

---
+++ KickOff.Tests/TestStage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace KickOff.Tests
+{
+	public class TestStage : Stage
+	{
+		private readonly Action<Stage> _onExecute;
+		private readonly Action<Stage> _onDispose;
+
+		public TestStage(Action<Stage> onExecute = null, Action<Stage> onDispose = null)
+		{
+			_onExecute = onExecute;
+			_onDispose = onDispose;
+		}
+
+		public override void Execute() => _onExecute?.Invoke(this);
+
+		public override void Dispose() => _onDispose?.Invoke(this);
+	}
+}
