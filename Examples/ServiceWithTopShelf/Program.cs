d7cea136eebdcf1b1b9bffb528db209893d67404
andy <andy@andydote.co.uk>
2016-11-09 20:18:25 +0200

    examples: add TopShelf based example service

---
+++ Examples/ServiceWithTopShelf/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using KickOff;
+using KickOff.Stages;
+using Topshelf;
+
+namespace ServiceWithTopShelf
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			HostFactory.Run(x =>
+			{
+				x.Service<Pipeline>(s =>
+				{
+					s.ConstructUsing(name => new Pipeline(new IStage[]
+					{
+						// new StructureMapStage(),
+						// ...
+						// ...
+						new RunnerStage(),
+					}));
+					s.WhenStarted(pipeline => pipeline.Execute(args));
+					s.WhenStopped(pipeline => pipeline.Dispose());
+				});
+			});
+		}
+	}
+
+	public class App : IStartup
+	{
+		public void Execute(ServiceArgs service)
+		{
+			Console.WriteLine("Running!");
+
+			while (service.CancelRequested == false)
+			{
+				Thread.Sleep(100);
+			}
+		}
+	}
+}
