d55a790aac2bfe3b156af14c78412ffe75a314c0
andy <andy@andydote.co.uk>
2016-11-19 15:55:04 +0200

    stageArgs: up the test coverage

--- src/KickOff.Tests/StageArgsTests.cs
+++ src/KickOff.Tests/StageArgsTests.cs
@@ -1,4 +1,5 @@
 ﻿using KickOff.Tests.TestInfrastructure;
+using NSubstitute;
 using Shouldly;
 using Xunit;
 
@@ -7,10 +8,12 @@ namespace KickOff.Tests
 	public class StageArgsTests
 	{
 		private readonly StageArgs _args;
+		private readonly PipelineCustomisation _customisations;
 
 		public StageArgsTests()
 		{
-			_args = new StageArgs(new PipelineCustomisation(), new string[0]);
+			_customisations = Substitute.For<PipelineCustomisation>();
+			_args = new StageArgs(_customisations, new string[0]);
 		}
 
 		[Fact]
@@ -19,6 +22,29 @@ namespace KickOff.Tests
 			_args
 				.TryGetInstance<IStartup>()
 				.ShouldBeOfType<TestApp>();
+		}
+
+		[Fact]
+		public void The_default_instance_factory_can_create_a_simple_object()
+		{
+			_args
+				.TryGetInstance<SomeDto>()
+				.ShouldNotBeNull();
+		}
+
+		[Fact]
+		public void Applying_Customisiations_call_is_passed_through()
+		{
+			var dto = new SomeDto();
+
+			_args.ApplyCustomisationTo(dto);
+
+			_customisations.Received().Apply(dto);
+		}
+
+		private class SomeDto
+		{
+			public int Value { get; set; }
 		}
 	}
 }
--- src/KickOff/PipelineCustomisation.cs
+++ src/KickOff/PipelineCustomisation.cs
@@ -33,7 +33,7 @@ namespace KickOff
 				});
 		}
 
-		public void Apply<TTarget>(TTarget customisationTarget)
+		public virtual void Apply<TTarget>(TTarget customisationTarget)
 		{
 			Action<object> apply;
 
